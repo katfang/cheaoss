fd69575b2d2192226ff9a877bc6c1a7431f93814
Kat Fang <katfang@leonis>
2025-04-30 13:21:48 -0400

    initializing board to all white kings
    * adding UNKNOWN to enum types Team & PieceType
    * added SortedMap as a way to store the pieces

--- backend/src/cheaoss_servicer.ts
+++ backend/src/cheaoss_servicer.ts
@@ -1,10 +1,16 @@
-import { ReaderContext, WriterContext } from "@reboot-dev/reboot";
+import { ReaderContext, TransactionContext, WriterContext } from "@reboot-dev/reboot";
 
 import {
   Cheaoss,
   AssignTeamRequest,
-  AssignTeamResponse,
+  InitGameRequest,
+  Team,
+  Piece,
+  PieceType,
 } from "../../api/cheaoss/v1/cheaoss_rbt.js";
+import { SortedMap } from "@reboot-dev/reboot-std/collections/sorted_map.js";
+
+const BOARD_SIZE = 1; 
 
 export class CheaossServicer extends Cheaoss.Servicer {
   async assignTeam(
@@ -13,8 +19,41 @@ export class CheaossServicer extends Cheaoss.Servicer {
     request: AssignTeamRequest 
   ) {
     console.log("team assignment", state.nextTeamAssignment);
-    const teamAssignment = state.nextTeamAssignment; 
-    state.nextTeamAssignment = (teamAssignment + 1) % 2;
-    return { team: teamAssignment };
+    // if team is unknown, set it to white, otherwise use the team as expected
+    const team = (state.nextTeamAssignment === Team.TEAM_UNKNOWN) ? Team.WHITE : state.nextTeamAssignment;
+    state.nextTeamAssignment = (team == Team.WHITE) ? Team.BLACK : Team.WHITE;
+    return { team: team };
+  }
+
+  async initGame(
+    context: TransactionContext,
+    state: Cheaoss.State,
+    request: InitGameRequest
+  ) {
+    const piecesMap = SortedMap.ref(context.stateId);
+    let entries: { [key:string] : Uint8Array } = {}; // ??? how do I actually do this creation & setting
+    let keys: string[] = []; 
+
+    // this should perhaps blow away anything that exists there, but eh. later. 
+    // console.log("teams are", Object.keys(Team)); // returns [ '0', '1', 'WHITE', 'BLACK' ]
+
+    // generate keys
+    for (let row: number = 0; row < 8*BOARD_SIZE; row++) {
+      for (let col: number = 0; col < 8*BOARD_SIZE; col++) {
+        keys.push(context.stateId + "-" + row + "-" + col);
+      }
+    }
+
+    keys.forEach(keyName => {
+      entries[keyName] = new Piece({
+        team: Team.WHITE,
+        type: PieceType.KING
+      }).toBinary();
+    });
+        
+    const resp = await piecesMap.insert(context, {entries: entries});
+    const get = await piecesMap.get(context, {key: keys[0]});
+
+    return {};
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,4 +1,7 @@
 import { Application, ExternalContext } from "@reboot-dev/reboot";
+import sortedMap, {
+  SortedMap,
+} from "@reboot-dev/reboot-std/collections/sorted_map.js";
 import { Hello } from "../../api/hello/v1/hello_rbt.js";
 import { HelloServicer } from "./hello_servicer.js";
 import { Cheaoss } from "../../api/cheaoss/v1/cheaoss_rbt.js";
@@ -13,12 +16,12 @@ const initialize = async (context: ExternalContext) => {
 
   const cheaoss = Cheaoss.ref("singleton");
   const cheaossResponse = await cheaoss
-    .idempotently()
-    .assignTeam(context);
-  console.log(cheaossResponse);
+    .unidempotently()
+    .initGame(context);
+  
 };
 
 new Application({
-  servicers: [HelloServicer, CheaossServicer],
+  servicers: [HelloServicer, CheaossServicer, ...sortedMap.servicers()],
   initialize,
 }).run();
