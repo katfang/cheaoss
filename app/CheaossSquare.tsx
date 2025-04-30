55d47a6ac22323d23326c1b99c66d0cb30523c93
Kat Fang <katfang@leonis>
2025-04-30 15:58:43 -0400

    trying to use SortedMap from React.

--- app/CheaossSquare.tsx
+++ app/CheaossSquare.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import { useSortedMap } from "@reboot-dev/reboot-std-react/sorted-map";
 import { PieceType } from "@/api/cheaoss/v1/cheaoss_pb";
 
 export default function CheaossSquare({ gameId, row, col, piece } : { gameId: string, row: number, col: number, piece?: PieceType }) {
   const squareColor = ((row + col) % 2 === 0 ? "bg-white-square" : "bg-black-square") + " w-full h-full";
+    useSortedMap({id: gameId});
 
   return (
     <div className={squareColor}>
-      P
     </div>
   );
 }
