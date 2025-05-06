6fdca23227547571ad364957562a45accb5e9bef
Kat Fang <katfang@leonis>
2025-05-06 12:50:17 -0400

    merged CheaossBoard & CheaossBoardStatic

--- app/CheaossBoard.tsx
+++ app/CheaossBoard.tsx
@@ -1,47 +1,74 @@
 "use client";
-import { useEffect } from "react";
+import { useState } from "react";
 import { useCheaoss, Piece, Location } from "../api/cheaoss/v1/cheaoss_rbt_react"
 
-import CheaossBoardStatic from "./CheaossBoardStatic";
-import Error from "./Error";
+import CheaossSquare from "./CheaossSquare";
+import { rcToLocKey, locToLocKey } from "./utils"
 
-export default function CheaossBoard({ gameId } : { gameId: string }) {
+export default function CheaossBoard({
+  gameId
+} : {
+  gameId: string
+}) {
   // TODO: probably pass from above?
-  const cheaossRef = useCheaoss({ id: "singleton" });
+  const cheaossRef = useCheaoss({ id: gameId });
   const boardRes = cheaossRef.useBoard();
   const boardPieces = cheaossRef.useBoardPieces();
+  const [startLoc, setStartLoc] = useState<Location | null>(null);
+  const [endLoc, setEndLoc] = useState<Location | null>(null);
 
-  if (boardPieces.response === undefined) {
-    return;
+  if (boardRes.response === undefined || boardPieces.response === undefined) {
+    return "still loading";
   }
 
-  function movePiece(pieceId: string, start:Location, end:Location) {
-    cheaossRef.movePiece({
-      player: "fake",
-      pieceId: pieceId,
-      start: start,
-      end: end
-    })
+  function selectSquare(loc: Location) {
+    if (startLoc === null) {
+      setStartLoc(loc);
+    } else if (endLoc === null) {
+      setEndLoc(loc);
+      let pieceId = locToPieceId.get(locToLocKey(startLoc));
+      if (pieceId !== undefined) {
+        cheaossRef.movePiece({
+          player: "fake",
+          pieceId: pieceId,
+          start: startLoc,
+          end: loc
+        });
+      }
+      // TODO: else, error
+    }
   }
 
   // TODO ??? is there a way of having this update by piece using usePiece, or nope, nah?
   let pieces = boardPieces.response?.pieces;
-  let newLocs = new Map<string, string>();
-  let newStates = new Map<string, Piece.State>();
+  let locToPieceId = new Map<string, string>();
+  let pieceIdToState = new Map<string, Piece.State>();
   for (let pieceId in pieces) {
-    newLocs.set(`${pieces[pieceId].loc?.row}-${pieces[pieceId].loc?.col}`, pieceId);
-    newStates.set(pieceId, pieces[pieceId]);
+    locToPieceId.set(`${pieces[pieceId].loc?.row}-${pieces[pieceId].loc?.col}`, pieceId);
+    pieceIdToState.set(pieceId, pieces[pieceId]);
   }
 
-  if (boardRes.response === undefined) {
-    return <Error message="Still working" />;
+  const squares = [];
+  for (let r = 7; r >= 0; r--) { // in chess, we want 0,0 to be the bottom left corner
+    for (let c = 0; c < 8; c++) {
+      let piece = pieceIdToState.get(
+        locToPieceId.get(rcToLocKey(r, c)) || ""
+      );
+      squares.push(<CheaossSquare
+        key={rcToLocKey(r, c)}
+        row={r}
+        col={c}
+        piece={piece}
+        onSelect={ (piece !== undefined || startLoc !== null) ? () => selectSquare(new Location({row: r, col: c})) : undefined }
+        isStart={startLoc !== null ? (startLoc.row == r && startLoc.col == c) : false}
+        isEnd={endLoc !== null ? (endLoc.row == r && endLoc.col == c) : false}
+      />);
+    }
   }
 
   return (
-    <CheaossBoardStatic
-      pieceIdToState={newStates}
-      locToPieceId={newLocs}
-      queueMove={movePiece}
-    />
+    <div className="grid grid-cols-[80px_80px_80px_80px_80px_80px_80px_80px] grid-rows-[80px_80px_80px_80px_80px_80px_80px_80px] items-center justify-items-center g-4">
+      {squares}
+    </div>
   );
 }
