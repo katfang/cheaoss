e14d3876574e6422ef8fc62681b754eac071d6d4
Kat Fang <katfang@leonis>
2025-05-06 12:36:24 -0400

    remove Hello code from FE

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,8 @@
 "use client";
-import { useHello } from "../api/hello/v1/hello_rbt_react";
 import CheaossMeta from "./CheaossMeta";
 import CheaossBoard from "./CheaossBoard";
 
 export default function Home() {
-  const { useMessages } = useHello({id: "reboot-hello"});
-  const { response } = useMessages();
-
-  if (response === undefined) {
-    return "Absolute failure";
-  }
-
-  const allMessages = response.messages.map((msg, id) =>
-    <p key={id}>{id}: {msg}</p>
-  );
-
   return (
     <div className="grid grid-cols-[1fr_1fr] grid-rows-[20px_1fr] items-center justify-items-center min-h-screen p-8 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div className="w-full col-span-2 bg-gray-500 p-4">
@@ -23,7 +11,9 @@ export default function Home() {
       <div className="w-full h-full bg-gray-500 p-4">
         <CheaossBoard gameId="singleton" />
       </div>
-      {allMessages} 
+      <div>
+        Other Sidebar
+      </div>
     </div>
   );
 }
