1d2ab1dd342b885ccf1881e3c0a52023306aafa3
Kat Fang <katfang@leonis>
2025-05-07 14:40:10 -0400

    BE: removing hello example

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,27 +1,15 @@
 import { Application, ExternalContext } from "@reboot-dev/reboot";
-import sortedMap, {
-  SortedMap,
-} from "@reboot-dev/reboot-std/collections/sorted_map.js";
-import { Hello } from "../../api/hello/v1/hello_rbt.js";
-import { HelloServicer } from "./hello_servicer.js";
 import { Cheaoss } from "../../api/cheaoss/v1/cheaoss_rbt.js";
 import { CheaossServicer, PieceServicer } from "./cheaoss_servicer.js";
 
 const initialize = async (context: ExternalContext) => {
-  const hello = Hello.ref("reboot-hello");
-
-  const response = await hello
-    .idempotently()
-    .send(context, { message: "This is a new world order." });
-
   const cheaoss = Cheaoss.ref("singleton");
-  const cheaossResponse = await cheaoss
+  await cheaoss
     .unidempotently()
     .initGame(context);
-  
 };
 
 new Application({
-  servicers: [HelloServicer, CheaossServicer, PieceServicer],
+  servicers: [CheaossServicer, PieceServicer],
   initialize,
 }).run();
